b31d2eac36ab4a2266712f978b7eea9592545b83
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2021-10-13 16:33:43 +0200

    add config transpiling functionality

--- clctranspiler/go.mod
+++ clctranspiler/go.mod
@@ -1,3 +1,5 @@
 module github.com/jepio/hackathon2021/clctranspiler
 
 go 1.16
+
+require github.com/kinvolk/container-linux-config-transpiler v0.9.1
--- clctranspiler/main.go
+++ clctranspiler/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/kinvolk/container-linux-config-transpiler/config"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +17,25 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world"))
 	} else {
 		body, _ := ioutil.ReadAll(r.Body)
-		w.Write(body)
+                log.Printf("%s\n", body)
+		cfg, ast, report := config.Parse(body)
+		if len(report.Entries) > 0 {
+			w.Write([]byte(report.String()))
+			return
+		}
+		ignCfg, report := config.Convert(cfg, "", ast)
+		if len(report.Entries) > 0 {
+			w.Write([]byte(report.String()))
+			if report.IsFatal() {
+				return
+			}
+		}
+		dataOut, err := json.MarshalIndent(&ignCfg, "", "  ")
+		dataOut = append(dataOut, '\n')
+		if err != nil {
+			log.Fatal("Failed to marshal output: %v", err)
+		}
+		w.Write([]byte(dataOut))
 	}
 }
 
